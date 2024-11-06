c869df88f0c031939c53a7a55ce52d32b0f00998
Jeninurapriliani <0076033873@smkn4padalarang.sch.id>
2024-11-06 07:53:51 +0700

    first

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$a=5; 
+$b=6;
+$c= $a+$b;
+echo "Hasil:".$c;
+?>
+
+<?php
+$angka1 = 6;
+$angka2 = 3;
+$operasi = "+"; //bisa diganti
+switch ($operasi) {
+    case "+" :
+      echo $angka2+$angka1;
+      break;
+    case "-" :
+      echo $angka2-$angka1;
+      break;
+    default;
+      echo "pilih salah satu operator";
+  }
+  ?>
+
+<?php
+class Produk {
+  public $sku = "050";
+  public $merek= "mixer";
+  public $harga= 250000;
+  public $jumlah= "10";
+  public function pesanProduk (){
+    $this->stok = ($this->stok -$this->)
+    return  "mixer cosmos";
+  }
+}
+
+
